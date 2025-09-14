885ea3df8ce18d4a06b9628d10d3bd9bcc6c8f5e
JanuszKossowski <jkossowski@cvd.pl>
2025-09-14 11:25:01 +0200

    Version bump

--- pkg/srad/version.go
+++ pkg/srad/version.go
@@ -10,4 +10,4 @@ package srad
 //	go test -ldflags "-X github.com/CVDpl/go-live-srad/pkg/srad.Version=1.0.3" ./...
 //
 // Default value follows SemVer.
-var Version = "1.0.4"
+var Version = "1.0.5"
