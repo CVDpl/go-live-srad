df6b66ce7f4124078f609b664ebc743a1d663e80
JanuszKossowski <jkossowski@cvd.pl>
2025-10-03 01:36:10 +0200

    Version bump

--- pkg/srad/version.go
+++ pkg/srad/version.go
@@ -3,12 +3,12 @@ package srad
 // Version is the semantic version of the srad library.
 // It can be overridden at build time using:
 //
-//	go build -ldflags "-X github.com/CVDpl/go-live-srad/pkg/srad.Version=1.2.0"
+//	go build -ldflags "-X github.com/CVDpl/go-live-srad/pkg/srad.Version=1.2.1"
 //
 // or:
 //
-//	go test -ldflags "-X github.com/CVDpl/go-live-srad/pkg/srad.Version=1.2.0" ./...
+//	go test -ldflags "-X github.com/CVDpl/go-live-srad/pkg/srad.Version=1.2.1" ./...
 //
 // Default value follows SemVer.
 
-var Version = "1.2.0"
+var Version = "1.2.1"
